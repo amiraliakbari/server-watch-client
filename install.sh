548eedc59e4bcd528896f6423623226de19becc1
AmirAli Akbari <akbari@arsh.co>
2013-11-01 01:28:44 +0330

    also installing cron job

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
+ln -s /usr/share/server-watch-client/cron.sh /etc/cron.d/server-watch-client
 mkdir /etc/server-watch-client/conf --mode=755 --parents
 cp /usr/share/server-watch-client/conf/scans.sample.py /etc/server-watch-client/conf/scans.py
 ln -s /etc/server-watch-client/conf/scans.py /usr/share/server-watch-client/conf/scans.py
